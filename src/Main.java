b7d4fb3de3d7b5c3e1d5e195969a7858c0e45d30
ademylmz34 <ademylmaz.97@gmail.com>
2023-01-08 00:25:23 +0300

    jar dosyası olusturuldu.

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,34 @@
+import java.awt.*;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException, InterruptedException {
         int id=0;
         Color clr = new Color();
         Kuyruk prosesler = new Kuyruk();
-        Scanner readFile;
-        try {
-            File data = new File(args[0]);
-            readFile = new Scanner(data);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+
+        File data = new File("src/giris.txt");
+        Scanner readFile = new Scanner(data);
+
         while (readFile.hasNextLine()) {
             String line = readFile.nextLine();
             String[] columns = line.split(", ");
+
 
             int varisZamani = Integer.parseInt(columns[0]);
             int oncelik = Integer.parseInt(columns[1]);
             int prosesSuresi = Integer.parseInt(columns[2]);
 
+
             prosesler.ekle(new Proses(id,varisZamani,oncelik,prosesSuresi, clr.getRandomColor()));
             id++;
 
         }
-        readFile.close();
-        Gorevlendirici gorevlendirici = new Gorevlendirici(prosesler);
-        gorevlendirici.baslat();
+        Gorevlendirici görevlendirici = new Gorevlendirici(prosesler);
+        görevlendirici.baslat();
 
     }
 }
